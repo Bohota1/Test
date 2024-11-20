2c25d4f0bec4d5ab1d16afece9064cd6e0699cdd
Bohota1 <newazbohota@gmail.com>
2024-11-20 15:51:27 +0600

    My Lab Task

---
+++ Lab Final OOP/src/Code/Topping.java
@@ -0,0 +1,20 @@
+package Code;
+
+public class Topping {
+    String name;
+    double price;
+
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ OOP LAB MID/src/Lab_07/LABEXAM_01/ReportScheduler.java
@@ -0,0 +1,10 @@
+package Lab_07.LABEXAM_01;
+
+package Lab_07.LABEXAM_01;
+
+class reportScheduler {
+    public static void scheduleReport(String reportType, String frequency) {
+        System.out.println("Scheduling " + reportType + " report with frequency " + frequency);
+    }
+}
+
